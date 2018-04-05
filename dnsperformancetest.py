6a952b7ac11a683f170c8900d96d933f94fe388b
The Morpheus Tutorials <Cedric.Moessner@gmx.de>
2018-04-05 11:00:50 +0200

    Add files via upload

---
+++ dnsperformancetest.py
@@ -0,0 +1,22 @@
+import dns.resolver
+import time
+
+dnsserver = ['1.1.1.1','4.2.2.1','8.8.8.8','9.9.9.9','80.80.80.80','208.67.222.123','199.85.126.20','185.228.168.168','77.88.8.7','176.103.130.132','156.154.70.3','8.26.56.26']
+
+domains = ['google.com', 'amazon.com', 'facebook.com', 'the-morpheus.de']
+
+for i in dnsserver:
+	avg = 0
+	for j in domains:
+		try:
+			resolv = dns.resolver.Resolver()
+			resolv.nameservers = [i]
+			resolv.timeout = 1
+			start = time.time()
+			result = resolv.query(j)
+			end = time.time()
+			avg = avg + (end-start)
+		except Exception as e:
+			avg = avg + 100000
+	avg = avg / len(domains)
+	print(i + "  -  " + str(avg))
